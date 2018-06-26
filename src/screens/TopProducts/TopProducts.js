ad656f2fd55916e3214e4a5ec150c8a673e630bf
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-06-26 22:32:38 +0530

    Added icons and side drawer to navigation

--- src/screens/TopProducts/TopProducts.js
+++ src/screens/TopProducts/TopProducts.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class TopProducts extends Component {
+    constructor (props) {
+        super(props);
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    }
+
+    onNavigatorEvent = event => {
+        if(event.type === "NavBarButtonPress") {
+            if(event.id === "sideDrawerToggle") {
+                this.props.navigator.toggleDrawer({
+                    side: "left",
+                    animated: true
+                });
+            }
+        }
+    }
+
     render () {
         return (
             <View>
